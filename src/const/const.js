b08c2ce3333d93550d73000b4077317648424719
Benjamin Correia <bcorreia2002@gmail.com>
2024-09-02 14:44:57 -0400

    Initial commit, files from Portfolio-Site

---
+++ src/const/const.js
@@ -0,0 +1,158 @@
+export const experience = [
+  {
+    title: "Undergraduate Teaching Assistant | Data Science",
+    corporation: "University of Massachusetts Amherst",
+    date: "09/2024 - Present",
+  },
+  {
+    title: "Machine Learning and Cybersecurity Intern",
+    corporation: "Dassault Systèmes | Solidworks",
+    date: "05/2024 - 08/2024",
+  },
+  {
+    title: "Real Estate Agent",
+    corporation: "Boston Pads | NextGen Realty",
+    date: "06/2021 - 06/2022",
+  },
+];
+export const education = [
+  {
+    school: "University of Massachusetts Amherst",
+    major: "Informatics B.S. | Data Science Concentration",
+    minor: "Minor in Computer Science",
+    gpa: "Major GPA: 3.8",
+  },
+]
+
+export const IconKeys = {
+  html: "html",
+  css: "css",
+  javaScript: "javaScript",
+  git: "git",
+  github: "github",
+  data: "data",
+  python: "python",
+  pandas:"pandas",
+  skl: "skl",
+  matplotlib: "matplotlib",
+  numpy: "numpy",
+  pytorch: "pytorch",
+  sql: "sql",
+  r: "r",
+  word: "word",
+  powerpoint: "powerpoint",
+  excel: "excel",
+  tableau: "tableau",
+  stats: "stats",
+  analytics: "analytics",
+  dataengineering: "dataengineering",
+  realestate: "realestate",
+  latex: "latex",
+  java: "java",
+  // here add more icon keys
+};
+
+export const skills = [
+  {
+    title: "Python",
+    icon: IconKeys.python
+  },
+  {
+    title: "Pandas",
+    icon: IconKeys.pandas
+  },
+  {
+    title: "Scikit-Learn",
+    icon: IconKeys.skl
+  },
+  {
+    title: "Numpy",
+    icon: IconKeys.numpy
+  },
+  {
+    title: "Pytorch",
+    icon: IconKeys.pytorch
+  },
+  {
+    title: "SQL",
+    icon: IconKeys.sql
+  },
+  {
+    title: "R",
+    icon: IconKeys.r
+  },
+  {
+    title: "MS Word",
+    icon: IconKeys.word
+  },
+  {
+    title: "MS Powerpoint",
+    icon: IconKeys.powerpoint
+  },
+  {
+    title: "MS Excel",
+    icon: IconKeys.excel
+  },
+  {
+    title: "Tableau",
+    icon: IconKeys.tableau
+  },
+  {
+    title: "Statistics",
+    icon: IconKeys.stats
+  },
+  {
+    title: "Data Analytics",
+    icon: IconKeys.analytics
+  },
+  {
+    title: "Data Engineering",
+    icon: IconKeys.dataengineering
+  },
+  {
+    title: "Real Estate",
+    icon: IconKeys.realestate
+  },
+  {
+    title: "LaTeX",
+    icon: IconKeys.latex
+  },
+  {
+    title: "Java",
+    icon: IconKeys.java
+  },
+  {
+    title: "Git",
+    icon: IconKeys.git,
+  },
+  {
+    title: "Github",
+    icon: IconKeys.github,
+  },
+  {
+    title: "Html",
+    icon: IconKeys.html,
+  },
+  {
+    title: "Css",
+    icon: IconKeys.css,
+  },
+  {
+    title: "JavaScript",
+    icon: IconKeys.javaScript,
+  },
+];
+
+export const projects = [
+  {
+    img: "../../../img/loan.jpg",
+    title: "Credit Risk Analysis",
+    link: "../../../pdf/credit_risk.pdf",
+    description: `Trained predictive models to predict financial trustworthiness based on debtors' credit metrics`,
+    technologies: [
+      {
+        icon: IconKeys.r
+      },
+    ],
+  },
+];
