976511ba29287513d870cf8e097ae60dd19305d1
Benjamin Correia <bcorreia2002@gmail.com>
2024-09-02 15:00:13 -0400

    capitalization change

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://AntonioCorr.github.io',
+  site: 'https://antoniocorr.github.io',
   base: '/Antoniocorr.github.io',
 })
